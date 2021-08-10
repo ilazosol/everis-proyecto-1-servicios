bb1d3ece0579ddfbc67fe632309185bfc2d28326
EduCCor <ecordovc@everis.com>
2021-08-10 11:12:16 -0500

    account stream

---
+++ springboot-stream-services/src/main/java/com/everis/springboot/stream/SpringbootStreamServicesApplication.java
@@ -0,0 +1,13 @@
+package com.everis.springboot.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStreamServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStreamServicesApplication.class, args);
+	}
+
+}
---
+++ springboot-stream-services/src/test/java/com/everis/springboot/stream/SpringbootStreamServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.springboot.stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStreamServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
